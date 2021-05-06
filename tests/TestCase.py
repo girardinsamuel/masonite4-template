d4cf6a0e4527cdd565926979702b3ae4d014b0c7
Joe Mancuso <idmann509@gmail.com>
2021-05-06 15:14:50 -0400

    added test case

---
+++ tests/TestCase.py
@@ -0,0 +1,18 @@
+from masonite.tests import TestCase
+from masonite.routes import Route, Router
+
+
+class TestCase(TestCase):
+    def setUp(self):
+        super().setUp()
+        self.application.bind(
+            "router",
+            Router(
+                Route.set_controller_module_location(
+                    "tests.integrations.controllers"
+                ).get("/", "WelcomeController@show"),
+                Route.set_controller_module_location(
+                    "tests.integrations.controllers"
+                ).post("/", "WelcomeController@show"),
+            ),
+        )
